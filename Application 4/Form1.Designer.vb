5037e8a112b049254e635659b328e13a5017f451
Marshi00 <112508169+Marshi00@users.noreply.github.com>
2022-12-12 10:50:33 -0500

    Add files via upload

---
+++ Application 4/Form1.Designer.vb
@@ -0,0 +1,224 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmArrays
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnSort = New System.Windows.Forms.Button()
+        Me.lblInfo = New System.Windows.Forms.Label()
+        Me.cmbLowerRange = New System.Windows.Forms.ComboBox()
+        Me.cmbUpperRange = New System.Windows.Forms.ComboBox()
+        Me.lblInfoLR = New System.Windows.Forms.Label()
+        Me.lblInfoUR = New System.Windows.Forms.Label()
+        Me.rdbOven1 = New System.Windows.Forms.RadioButton()
+        Me.rdbOven2 = New System.Windows.Forms.RadioButton()
+        Me.rdbAll = New System.Windows.Forms.RadioButton()
+        Me.grpOven = New System.Windows.Forms.GroupBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lsbResult = New System.Windows.Forms.ListBox()
+        Me.btnWriteData = New System.Windows.Forms.Button()
+        Me.btnLogErrors = New System.Windows.Forms.Button()
+        Me.grpOven.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(10, 288)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(137, 39)
+        Me.btnDisplay.TabIndex = 0
+        Me.btnDisplay.Text = "Display Data"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnSort
+        '
+        Me.btnSort.Location = New System.Drawing.Point(10, 342)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(137, 39)
+        Me.btnSort.TabIndex = 2
+        Me.btnSort.Text = "Sort Data"
+        Me.btnSort.UseVisualStyleBackColor = True
+        '
+        'lblInfo
+        '
+        Me.lblInfo.AutoSize = True
+        Me.lblInfo.Location = New System.Drawing.Point(7, 53)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(162, 13)
+        Me.lblInfo.TabIndex = 3
+        Me.lblInfo.Text = "Please Select the Display Range"
+        '
+        'cmbLowerRange
+        '
+        Me.cmbLowerRange.FormattingEnabled = True
+        Me.cmbLowerRange.Location = New System.Drawing.Point(211, 127)
+        Me.cmbLowerRange.Name = "cmbLowerRange"
+        Me.cmbLowerRange.Size = New System.Drawing.Size(148, 21)
+        Me.cmbLowerRange.TabIndex = 4
+        '
+        'cmbUpperRange
+        '
+        Me.cmbUpperRange.FormattingEnabled = True
+        Me.cmbUpperRange.Location = New System.Drawing.Point(211, 199)
+        Me.cmbUpperRange.Name = "cmbUpperRange"
+        Me.cmbUpperRange.Size = New System.Drawing.Size(148, 21)
+        Me.cmbUpperRange.TabIndex = 5
+        '
+        'lblInfoLR
+        '
+        Me.lblInfoLR.AutoSize = True
+        Me.lblInfoLR.Location = New System.Drawing.Point(12, 130)
+        Me.lblInfoLR.Name = "lblInfoLR"
+        Me.lblInfoLR.Size = New System.Drawing.Size(157, 13)
+        Me.lblInfoLR.TabIndex = 6
+        Me.lblInfoLR.Text = "Please Select the Lower Range"
+        '
+        'lblInfoUR
+        '
+        Me.lblInfoUR.AutoSize = True
+        Me.lblInfoUR.Location = New System.Drawing.Point(12, 202)
+        Me.lblInfoUR.Name = "lblInfoUR"
+        Me.lblInfoUR.Size = New System.Drawing.Size(157, 13)
+        Me.lblInfoUR.TabIndex = 7
+        Me.lblInfoUR.Text = "Please Select the Upper Range"
+        '
+        'rdbOven1
+        '
+        Me.rdbOven1.AutoSize = True
+        Me.rdbOven1.Location = New System.Drawing.Point(26, 18)
+        Me.rdbOven1.Name = "rdbOven1"
+        Me.rdbOven1.Size = New System.Drawing.Size(60, 17)
+        Me.rdbOven1.TabIndex = 8
+        Me.rdbOven1.TabStop = True
+        Me.rdbOven1.Text = "Oven 1"
+        Me.rdbOven1.UseVisualStyleBackColor = True
+        '
+        'rdbOven2
+        '
+        Me.rdbOven2.AutoSize = True
+        Me.rdbOven2.Location = New System.Drawing.Point(26, 41)
+        Me.rdbOven2.Name = "rdbOven2"
+        Me.rdbOven2.Size = New System.Drawing.Size(60, 17)
+        Me.rdbOven2.TabIndex = 9
+        Me.rdbOven2.TabStop = True
+        Me.rdbOven2.Text = "Oven 2"
+        Me.rdbOven2.UseVisualStyleBackColor = True
+        '
+        'rdbAll
+        '
+        Me.rdbAll.AutoSize = True
+        Me.rdbAll.Location = New System.Drawing.Point(26, 64)
+        Me.rdbAll.Name = "rdbAll"
+        Me.rdbAll.Size = New System.Drawing.Size(36, 17)
+        Me.rdbAll.TabIndex = 10
+        Me.rdbAll.TabStop = True
+        Me.rdbAll.Text = "All"
+        Me.rdbAll.UseVisualStyleBackColor = True
+        '
+        'grpOven
+        '
+        Me.grpOven.Controls.Add(Me.rdbAll)
+        Me.grpOven.Controls.Add(Me.rdbOven2)
+        Me.grpOven.Controls.Add(Me.rdbOven1)
+        Me.grpOven.Location = New System.Drawing.Point(211, 12)
+        Me.grpOven.Name = "grpOven"
+        Me.grpOven.Size = New System.Drawing.Size(138, 102)
+        Me.grpOven.TabIndex = 11
+        Me.grpOven.TabStop = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(89, 399)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(137, 39)
+        Me.btnExit.TabIndex = 12
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lsbResult
+        '
+        Me.lsbResult.FormattingEnabled = True
+        Me.lsbResult.Location = New System.Drawing.Point(380, 12)
+        Me.lsbResult.Name = "lsbResult"
+        Me.lsbResult.Size = New System.Drawing.Size(406, 407)
+        Me.lsbResult.TabIndex = 13
+        '
+        'btnWriteData
+        '
+        Me.btnWriteData.Location = New System.Drawing.Point(192, 288)
+        Me.btnWriteData.Name = "btnWriteData"
+        Me.btnWriteData.Size = New System.Drawing.Size(137, 39)
+        Me.btnWriteData.TabIndex = 14
+        Me.btnWriteData.Text = "Write to Data Files "
+        Me.btnWriteData.UseVisualStyleBackColor = True
+        '
+        'btnLogErrors
+        '
+        Me.btnLogErrors.Location = New System.Drawing.Point(192, 342)
+        Me.btnLogErrors.Name = "btnLogErrors"
+        Me.btnLogErrors.Size = New System.Drawing.Size(137, 39)
+        Me.btnLogErrors.TabIndex = 15
+        Me.btnLogErrors.Text = "Log Errors"
+        Me.btnLogErrors.UseVisualStyleBackColor = True
+        '
+        'frmArrays
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnLogErrors)
+        Me.Controls.Add(Me.btnWriteData)
+        Me.Controls.Add(Me.lsbResult)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.grpOven)
+        Me.Controls.Add(Me.lblInfoUR)
+        Me.Controls.Add(Me.lblInfoLR)
+        Me.Controls.Add(Me.cmbUpperRange)
+        Me.Controls.Add(Me.cmbLowerRange)
+        Me.Controls.Add(Me.lblInfo)
+        Me.Controls.Add(Me.btnSort)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Name = "frmArrays"
+        Me.Text = "Form1"
+        Me.grpOven.ResumeLayout(False)
+        Me.grpOven.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnSort As Button
+    Friend WithEvents lblInfo As Label
+    Friend WithEvents cmbLowerRange As ComboBox
+    Friend WithEvents cmbUpperRange As ComboBox
+    Friend WithEvents lblInfoLR As Label
+    Friend WithEvents lblInfoUR As Label
+    Friend WithEvents rdbOven1 As RadioButton
+    Friend WithEvents rdbOven2 As RadioButton
+    Friend WithEvents rdbAll As RadioButton
+    Friend WithEvents grpOven As GroupBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lsbResult As ListBox
+    Friend WithEvents btnWriteData As Button
+    Friend WithEvents btnLogErrors As Button
+End Class
---
+++ Application 4/Form1.vb
@@ -0,0 +1,346 @@
+﻿Option Explicit On
+Option Strict On
+Imports System.IO
+Public Class frmArrays
+    Dim lowerRange, upperRange As Integer
+    Dim displayTarget As Integer = 0
+    Dim i As Integer = 0
+    Dim k As Integer = 0
+    Dim z As Integer = 0
+    Dim fileChooser As New OpenFileDialog
+    Dim folderChooser As New FolderBrowserDialog
+    Dim robotFileReader As StreamReader
+    Dim robotFileWriter As StreamWriter
+    Dim fileName, saveLocation, errorSaveLocation As String
+    Dim ovenOneArray(), ovenTwoArray(), allArray() As String
+    'this is where we select the lower Range / Combo boxes update base on each other
+    Private Sub cmbLowerRange_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbLowerRange.Click
+        cmbLowerRange.Items.Clear()
+        If cmbUpperRange.Text <> "" Then
+            For q = 0 To CDbl(cmbUpperRange.SelectedItem) - 100 Step 100
+                cmbLowerRange.Items.Add(q)
+            Next
+        Else
+            For t = 0 To 999 Step 100
+                cmbLowerRange.Items.Add(t)
+            Next
+        End If
+    End Sub
+    'this is where we select the higher range / COmbo boxes update base on each other
+    Private Sub cmbUpperRange_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbUpperRange.Click
+        cmbUpperRange.Items.Clear()
+        If cmbLowerRange.Text <> "" Then
+            For p = CDbl(cmbLowerRange.SelectedItem) + 100 To 1000 Step 100
+                cmbUpperRange.Items.Add(p)
+            Next
+        Else
+            For f = 100 To 1000 Step 100
+                cmbUpperRange.Items.Add(f)
+            Next
+        End If
+    End Sub
+    'this is the form load ( not allowing the form to run untill temp file is selected ) 
+    Private Sub frmArrays_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Do While fileName = ""
+            MsgBox("Please locate the Temps file to run to form")
+            fileSet()
+        Loop
+        Try
+            'putting every single line into an array
+            linkClose()
+            robotFileReader = New StreamReader(fileName)
+            Do While Not robotFileReader.EndOfStream
+                ReDim Preserve allArray(i)
+                allArray(i) = robotFileReader.ReadLine
+                i += 1
+            Loop
+            linkClose()
+            'putting the lines that start with 1 in oven 1 array
+            For j = 0 To allArray.GetUpperBound(0)
+                Dim numberOfNumbers() As Char = Trim(allArray(j)).ToCharArray
+                If numberOfNumbers(0) = "1" Then
+                    ReDim Preserve ovenOneArray(k)
+                    ovenOneArray(k) = allArray(j)
+                    k += 1
+                    'putting the lines that start with 2 in oven2 array
+                ElseIf numberOfNumbers(0) = "2" Then
+                    ReDim Preserve ovenTwoArray(z)
+                    ovenTwoArray(z) = allArray(j)
+                    z += 1
+                End If
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+    'this part check if path has been not set before and set a new path
+    Sub fileSet()
+        If fileName = "" Then
+            Dim result As DialogResult = fileChooser.ShowDialog()
+            If result = Windows.Forms.DialogResult.OK Then
+                fileName = fileChooser.FileName
+            End If
+        End If
+    End Sub
+    'this  is where we save files for oven data/ pick location 
+    Sub savePath()
+        If saveLocation = "" Then
+            Dim target As DialogResult = folderChooser.ShowDialog()
+            If target = Windows.Forms.DialogResult.OK Then
+                saveLocation = folderChooser.SelectedPath
+            End If
+        End If
+    End Sub
+    'this  is where we save files for errorlog / pick location 
+    Sub errorSavePath()
+        If errorSaveLocation = "" Then
+            Dim target2 As DialogResult = folderChooser.ShowDialog()
+            If target2 = Windows.Forms.DialogResult.OK Then
+                errorSaveLocation = folderChooser.SelectedPath
+            End If
+        End If
+    End Sub
+    'this close active links 
+    Sub linkClose()
+        If robotFileWriter IsNot Nothing Then
+            robotFileWriter.Close()
+        End If
+        If robotFileReader IsNot Nothing Then
+            robotFileReader.Close()
+        End If
+    End Sub
+    'close button get clicked
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+    'this check if oven 1 is checked
+    Private Sub rdbOven1_CheckedChanged(sender As Object, e As EventArgs) Handles rdbOven1.CheckedChanged
+        If rdbOven1.Checked = True Then
+            displayTarget = 2
+        End If
+    End Sub
+    'this check if oven 2 is checked
+    Private Sub rdbOven2_CheckedChanged(sender As Object, e As EventArgs) Handles rdbOven2.CheckedChanged
+        If rdbOven2.Checked = True Then
+            displayTarget = 3
+        End If
+    End Sub
+    'this check if if all radio button is checked
+    Private Sub rdbAll_CheckedChanged(sender As Object, e As EventArgs) Handles rdbAll.CheckedChanged
+        If rdbAll.Checked = True Then
+            displayTarget = 1
+        End If
+    End Sub
+    'this is for title
+    Sub listBoxTitle()
+        lsbResult.Items.Add(String.Format("--------- Opening {0} ------------", fileName))
+        lsbResult.Items.Add(" ")
+    End Sub
+    'this is when display button get clicked
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        If displayTarget <> 0 Then
+            If cmbLowerRange.Text <> "" Then
+                If cmbUpperRange.Text <> "" Then
+                    lowerRange = CInt(cmbLowerRange.SelectedItem)
+                    upperRange = CInt(cmbUpperRange.SelectedItem)
+                    Try
+                        Select Case displayTarget
+                            Case 1
+                                fileSet()
+                                If fileName <> "" Then
+                                    lsbResult.Items.Clear()
+                                    listBoxTitle()
+                                    For Each item In allArray
+                                        Dim numberOfNumbers() As Char = item.ToCharArray
+                                        Dim mustTemp As String = numberOfNumbers(1) & numberOfNumbers(2) & numberOfNumbers(3)
+                                        If CInt(mustTemp) > lowerRange And CInt(mustTemp) < upperRange Then
+                                            lsbResult.Items.Add(item)
+                                        End If
+                                    Next
+                                End If
+                            Case 2
+                                fileSet()
+                                If fileName <> "" Then
+                                    lsbResult.Items.Clear()
+                                    listBoxTitle()
+                                    For Each item In ovenOneArray
+                                        Dim numberOfNumbers() As Char = item.ToCharArray
+                                        Dim mustTemp As String = numberOfNumbers(1) & numberOfNumbers(2) & numberOfNumbers(3)
+                                        If CInt(mustTemp) > lowerRange And CInt(mustTemp) < upperRange Then
+                                            lsbResult.Items.Add(item)
+                                        End If
+                                    Next
+                                End If
+                            Case 3
+                                If fileName <> "" Then
+                                    lsbResult.Items.Clear()
+                                    listBoxTitle()
+                                    For Each item In ovenTwoArray
+                                        Dim numberOfNumbers() As Char = item.ToCharArray
+                                        Dim mustTemp As String = numberOfNumbers(1) & numberOfNumbers(2) & numberOfNumbers(3)
+                                        If CInt(mustTemp) > lowerRange And CInt(mustTemp) < upperRange Then
+                                            lsbResult.Items.Add(item)
+                                        End If
+                                    Next
+                                End If
+                        End Select
+                    Catch ex As Exception
+                        MessageBox.Show(ex.Message)
+                    End Try
+                Else
+                    MsgBox("Please Select the upper range ")
+                End If
+            Else
+                MsgBox("Please Select the lower range ")
+            End If
+
+        Else
+            MsgBox("Please Select what to display first ")
+        End If
+    End Sub
+    'this is where save data get clicked 
+    Private Sub btnWriteData_Click(sender As Object, e As EventArgs) Handles btnWriteData.Click
+        Try
+            savePath()
+            If saveLocation <> "" Then
+                Dim Oven1Path = saveLocation & "\Oven1.txt"
+                Dim Oven2Path = saveLocation & "\Oven2.txt"
+                linkClose()
+                robotFileWriter = New StreamWriter(Oven1Path, True)
+                'text file for oven 1 Data
+                For Each item In ovenOneArray
+                    robotFileWriter.WriteLine(item)
+                Next
+                linkClose()
+                'text file for oven 2 Data
+                robotFileWriter = New StreamWriter(Oven2Path, True)
+                For Each item In ovenTwoArray
+                    robotFileWriter.WriteLine(item)
+                Next
+                linkClose()
+            Else
+                MsgBox("Please Select Where to save the files")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+    'this is where sort button get Clicked
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+        If displayTarget <> 0 Then
+            If cmbLowerRange.Text <> "" Then
+                If cmbUpperRange.Text <> "" Then
+                    lowerRange = CInt(cmbLowerRange.SelectedItem)
+                    upperRange = CInt(cmbUpperRange.SelectedItem)
+                    Try
+                        Select Case displayTarget
+                            Case 1
+                                fileSet()
+                                If fileName <> "" Then
+                                    lsbResult.Items.Clear()
+                                    listBoxTitle()
+                                    Dim listToSort As New List(Of Tuple(Of String, String))
+                                    For Each item In allArray
+                                        Dim numberOfNumbers() As Char = item.ToCharArray
+                                        Dim mustTemp As String = numberOfNumbers(1) & numberOfNumbers(2) & numberOfNumbers(3)
+                                        If CInt(mustTemp) > lowerRange And CInt(mustTemp) < upperRange Then
+                                            listToSort.Add(Tuple.Create(item, mustTemp))
+                                        End If
+                                    Next
+                                    listToSort = listToSort.OrderBy(Function(i) i.Item2).ToList
+                                    For Each tpl As Tuple(Of String, String) In listToSort
+                                        lsbResult.Items.Add(tpl.Item1)
+                                    Next
+                                End If
+                            Case 2
+                                fileSet()
+                                If fileName <> "" Then
+                                    lsbResult.Items.Clear()
+                                    listBoxTitle()
+                                    Dim listToSort As New List(Of Tuple(Of String, String))
+                                    For Each item In ovenOneArray
+                                        Dim numberOfNumbers() As Char = item.ToCharArray
+                                        Dim mustTemp As String = numberOfNumbers(1) & numberOfNumbers(2) & numberOfNumbers(3)
+                                        If CInt(mustTemp) > lowerRange And CInt(mustTemp) < upperRange Then
+                                            listToSort.Add(Tuple.Create(item, mustTemp))
+                                        End If
+                                    Next
+                                    listToSort = listToSort.OrderBy(Function(i) i.Item2).ToList
+                                    For Each tpl As Tuple(Of String, String) In listToSort
+                                        lsbResult.Items.Add(tpl.Item1)
+                                    Next
+                                End If
+                            Case 3
+                                If fileName <> "" Then
+                                    lsbResult.Items.Clear()
+                                    listBoxTitle()
+                                    Dim listToSort As New List(Of Tuple(Of String, String))
+                                    For Each item In ovenTwoArray
+                                        Dim numberOfNumbers() As Char = item.ToCharArray
+                                        Dim mustTemp As String = numberOfNumbers(1) & numberOfNumbers(2) & numberOfNumbers(3)
+                                        If CInt(mustTemp) > lowerRange And CInt(mustTemp) < upperRange Then
+                                            listToSort.Add(Tuple.Create(item, mustTemp))
+                                        End If
+                                    Next
+                                    listToSort = listToSort.OrderBy(Function(i) i.Item2).ToList
+                                    For Each tpl As Tuple(Of String, String) In listToSort
+                                        lsbResult.Items.Add(tpl.Item1)
+                                    Next
+                                End If
+                        End Select
+                    Catch ex As Exception
+                        MessageBox.Show(ex.Message)
+                    End Try
+                Else
+                    MsgBox("Please Select the upper range ")
+                End If
+            Else
+                MsgBox("Please Select the lower range ")
+            End If
+
+        Else
+            MsgBox("Please Select what to display first ")
+        End If
+    End Sub
+    'this is where error logs get clicked
+    Private Sub btnLogErrors_Click(sender As Object, e As EventArgs) Handles btnLogErrors.Click
+        Try
+            errorSavePath()
+            If errorSaveLocation <> "" Then
+                Dim erorOven1Path = errorSaveLocation & "\errorLogOven1.txt"
+                Dim errorOven2Path = errorSaveLocation & "\errorLogOven2.txt"
+                linkClose()
+                robotFileWriter = New StreamWriter(erorOven1Path, True)
+                'text file for oven 1 Data
+                For Each item In ovenOneArray
+                    Dim numberOfErrors() As Char = item.ToCharArray
+                    Dim mustTemp As String = numberOfErrors(1) & numberOfErrors(2) & numberOfErrors(3)
+                    Dim currentTemp As String = numberOfErrors(6) & numberOfErrors(7) & numberOfErrors(8)
+                    Dim errorGap As Integer
+                    errorGap = CInt(mustTemp) - CInt(currentTemp)
+                    If errorGap > 3 Or errorGap < -3 Then
+                        robotFileWriter.WriteLine(item & " " & errorGap)
+                    End If
+                Next
+                linkClose()
+                'text file for oven 2 Data
+                robotFileWriter = New StreamWriter(errorOven2Path, True)
+                For Each item In ovenTwoArray
+                    Dim numberOfErrors() As Char = item.ToCharArray
+                    Dim mustTemp As String = numberOfErrors(1) & numberOfErrors(2) & numberOfErrors(3)
+                    Dim currentTemp As String = numberOfErrors(6) & numberOfErrors(7) & numberOfErrors(8)
+                    Dim errorGap As Integer
+                    errorGap = CInt(mustTemp) - CInt(currentTemp)
+                    If errorGap > 3 Or errorGap < -3 Then
+                        robotFileWriter.WriteLine(item & " " & errorGap)
+                    End If
+                Next
+                linkClose()
+            Else
+                MsgBox("Please Select Where to save the files")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
