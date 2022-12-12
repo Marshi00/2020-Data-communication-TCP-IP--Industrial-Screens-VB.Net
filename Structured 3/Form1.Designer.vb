62c7a80884a805b21b1b3be136b599833be377ed
Marshi00 <112508169+Marshi00@users.noreply.github.com>
2022-12-12 11:06:25 -0500

    Add files via upload

---
+++ Structured 3/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRobotArm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblInfoX = New System.Windows.Forms.Label()
+        Me.lblInfoZ = New System.Windows.Forms.Label()
+        Me.lblInfoY = New System.Windows.Forms.Label()
+        Me.txtCoordinationX = New System.Windows.Forms.TextBox()
+        Me.txtCoordinationZ = New System.Windows.Forms.TextBox()
+        Me.txtCoordinationY = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lsbResult = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'lblInfoX
+        '
+        Me.lblInfoX.Location = New System.Drawing.Point(12, 31)
+        Me.lblInfoX.Name = "lblInfoX"
+        Me.lblInfoX.Size = New System.Drawing.Size(144, 23)
+        Me.lblInfoX.TabIndex = 1
+        Me.lblInfoX.Text = "Please enter X coordination"
+        '
+        'lblInfoZ
+        '
+        Me.lblInfoZ.Location = New System.Drawing.Point(12, 153)
+        Me.lblInfoZ.Name = "lblInfoZ"
+        Me.lblInfoZ.Size = New System.Drawing.Size(144, 23)
+        Me.lblInfoZ.TabIndex = 2
+        Me.lblInfoZ.Text = "Please enter Z coordination"
+        '
+        'lblInfoY
+        '
+        Me.lblInfoY.Location = New System.Drawing.Point(12, 93)
+        Me.lblInfoY.Name = "lblInfoY"
+        Me.lblInfoY.Size = New System.Drawing.Size(144, 23)
+        Me.lblInfoY.TabIndex = 3
+        Me.lblInfoY.Text = "Please enter Y coordination"
+        '
+        'txtCoordinationX
+        '
+        Me.txtCoordinationX.Location = New System.Drawing.Point(207, 28)
+        Me.txtCoordinationX.Name = "txtCoordinationX"
+        Me.txtCoordinationX.Size = New System.Drawing.Size(133, 20)
+        Me.txtCoordinationX.TabIndex = 4
+        '
+        'txtCoordinationZ
+        '
+        Me.txtCoordinationZ.Location = New System.Drawing.Point(207, 153)
+        Me.txtCoordinationZ.Name = "txtCoordinationZ"
+        Me.txtCoordinationZ.Size = New System.Drawing.Size(133, 20)
+        Me.txtCoordinationZ.TabIndex = 5
+        '
+        'txtCoordinationY
+        '
+        Me.txtCoordinationY.Location = New System.Drawing.Point(207, 93)
+        Me.txtCoordinationY.Name = "txtCoordinationY"
+        Me.txtCoordinationY.Size = New System.Drawing.Size(133, 20)
+        Me.txtCoordinationY.TabIndex = 6
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(15, 243)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(141, 76)
+        Me.btnCalculate.TabIndex = 8
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(207, 243)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(141, 76)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lsbResult
+        '
+        Me.lsbResult.FormattingEnabled = True
+        Me.lsbResult.Location = New System.Drawing.Point(372, 25)
+        Me.lsbResult.Name = "lsbResult"
+        Me.lsbResult.Size = New System.Drawing.Size(375, 290)
+        Me.lsbResult.TabIndex = 10
+        '
+        'frmRobotArm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 336)
+        Me.Controls.Add(Me.lsbResult)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtCoordinationY)
+        Me.Controls.Add(Me.txtCoordinationZ)
+        Me.Controls.Add(Me.txtCoordinationX)
+        Me.Controls.Add(Me.lblInfoY)
+        Me.Controls.Add(Me.lblInfoZ)
+        Me.Controls.Add(Me.lblInfoX)
+        Me.Name = "frmRobotArm"
+        Me.Text = "RobotArm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblInfoX As Label
+    Friend WithEvents lblInfoZ As Label
+    Friend WithEvents lblInfoY As Label
+    Friend WithEvents txtCoordinationX As TextBox
+    Friend WithEvents txtCoordinationZ As TextBox
+    Friend WithEvents txtCoordinationY As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lsbResult As ListBox
+End Class
---
+++ Structured 3/Form1.vb
@@ -0,0 +1,100 @@
+﻿Option Strict On
+Option Explicit On
+Public Class frmRobotArm
+    Dim xCord, yCord, Zcord, liftResult, rotationResult, lengthResult As Single
+    Dim validationCode As Integer
+    'This part is to calculate the Arm Length 
+    Function lengthCalculator(ByVal inputX As Single, ByVal inputY As Single, ByVal inputZ As Single) As Single
+        Dim lengthCalculatorResult As Single
+        lengthCalculatorResult = CSng(Math.Round(Math.Sqrt((inputX ^ 2) + (inputY ^ 2) + (inputZ ^ 2)), 2))
+        lengthCalculator = lengthCalculatorResult
+    End Function
+    'This part is to calculate the rotation
+    Function rotationCalculator(ByVal inputX As Single, ByVal inputY As Single) As Single
+        Dim rotationCalculatorResult As Single
+        rotationCalculatorResult = CSng(Math.Round((Math.Atan(inputY / inputX)) * (180 / Math.PI), 1))
+        rotationCalculator = rotationCalculatorResult
+    End Function
+
+
+    'This part is to calculate the lift
+    Function liftCalculator(ByVal inputX As Single, ByVal inputY As Single, ByVal inputZ As Single) As Single
+        Dim liftCalculatorResult As Single
+        liftCalculatorResult = CSng(Math.Round((Math.Atan(inputZ / (Math.Sqrt((inputX ^ 2) + (inputY ^ 2)))) * (180 / Math.PI)), 1))
+        liftCalculator = liftCalculatorResult
+    End Function
+    'This part is to check errors
+    Function validation(ByVal inputX As String, ByVal inputY As String, ByVal inputZ As String) As Integer
+        Dim validationResult As Integer
+        'Checking inputs are not empty
+        If inputX <> "" And inputY <> "" And inputZ <> "" Then
+            'this part checking the input is not numeric 
+            If IsNumeric(inputX) And IsNumeric(inputY) And IsNumeric(inputZ) Then
+                xCord = CSng(inputX)
+                yCord = CSng(inputY)
+                Zcord = CSng(inputZ)
+                lengthResult = lengthCalculator(xCord, yCord, Zcord)
+                rotationResult = rotationCalculator(xCord, yCord)
+                liftResult = liftCalculator(xCord, yCord, Zcord)
+                'this part checking the input is not nagetive 
+                If xCord < 0 Or yCord < 0 Or Zcord < 0 Then
+                    validationResult = -2
+                    'this part is to check the length is in range 
+                ElseIf lengthResult < 10 Or lengthResult > 25 Then
+                    validationResult = -3
+                    'this part is to check the rotation angle is in range 
+                ElseIf rotationResult < -90 Or rotationResult > 90 Then
+                    validationResult = -4
+                    'this part is to check the lift angle is in range
+                ElseIf liftResult < -10 Or liftResult > 45 Then
+                    validationResult = -5
+                Else
+                    validationResult = 1
+                End If
+            Else
+                validationResult = -1
+            End If
+        Else
+            validationResult = 2
+        End If
+        validation = validationResult
+    End Function
+    'This part is to show the needed Error Msg 
+    Sub errorMsg(ByVal inputMsgCode As Integer)
+        Dim msgNote As String = ""
+        Select Case inputMsgCode
+            Case 1
+                msgNote = "all values are valid"
+            Case -1
+                msgNote = "the input for X,Y,Z must be numeric"
+            Case -2
+                msgNote = "the input for X,Y,Z can not be negative"
+            Case -3
+                msgNote = "the length is not between 10 and 25 inches "
+            Case -4
+                msgNote = "the rotation angle  is not between -90 and 90"
+            Case -5
+                msgNote = "the lift angle  is not between -10 and 45"
+            Case 2
+                msgNote = " coordination for X, Y & Z can not be empty"
+        End Select
+        MsgBox(msgNote)
+    End Sub
+    'this is where Calculate button gets clicked 
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        validationCode = validation(txtCoordinationX.Text, txtCoordinationY.Text, txtCoordinationZ.Text)
+        If validationCode = 1 Or validationCode < -2 Then
+            lsbResult.Items.Add("X" & vbTab & "Y" & vbTab & "Z" & vbTab & "Length" & vbTab & "Rotation" & vbTab & "Lift ")
+            lsbResult.Items.Add(xCord & vbTab & yCord & vbTab & Zcord & vbTab & lengthResult & vbTab & rotationResult & vbTab & liftResult)
+            lsbResult.Items.Add("---------------------------------------------------------------------------------------------------------------")
+            lsbResult.Items.Add("         ------------------------------------------------------------------------------------------")
+            errorMsg(validationCode)
+        Else
+            errorMsg(validationCode)
+        End If
+    End Sub
+    'This part is to close the program 
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
