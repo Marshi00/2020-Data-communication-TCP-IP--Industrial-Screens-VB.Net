817dde7c3353e1ccd2fbdd12100a593797c94e62
Marshi00 <112508169+Marshi00@users.noreply.github.com>
2022-12-12 10:51:37 -0500

    Add files via upload

---
+++ Application 3/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDataStorage
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblInfoDirectory = New System.Windows.Forms.Label()
+        Me.txtInputDirectoryName = New System.Windows.Forms.TextBox()
+        Me.btnDirectory = New System.Windows.Forms.Button()
+        Me.btnMove = New System.Windows.Forms.Button()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.lsbResult = New System.Windows.Forms.ListBox()
+        Me.btnAnalyse = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblAnalyseResult = New System.Windows.Forms.Label()
+        Me.SaveAnakysis = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblInfoDirectory
+        '
+        Me.lblInfoDirectory.AutoSize = True
+        Me.lblInfoDirectory.Location = New System.Drawing.Point(22, 22)
+        Me.lblInfoDirectory.Name = "lblInfoDirectory"
+        Me.lblInfoDirectory.Size = New System.Drawing.Size(280, 13)
+        Me.lblInfoDirectory.TabIndex = 0
+        Me.lblInfoDirectory.Text = "Please enter the name of directory that you want to create"
+        '
+        'txtInputDirectoryName
+        '
+        Me.txtInputDirectoryName.Location = New System.Drawing.Point(324, 15)
+        Me.txtInputDirectoryName.Name = "txtInputDirectoryName"
+        Me.txtInputDirectoryName.Size = New System.Drawing.Size(235, 20)
+        Me.txtInputDirectoryName.TabIndex = 2
+        '
+        'btnDirectory
+        '
+        Me.btnDirectory.Location = New System.Drawing.Point(578, 8)
+        Me.btnDirectory.Name = "btnDirectory"
+        Me.btnDirectory.Size = New System.Drawing.Size(96, 41)
+        Me.btnDirectory.TabIndex = 3
+        Me.btnDirectory.Text = "Create Directory"
+        Me.btnDirectory.UseVisualStyleBackColor = True
+        '
+        'btnMove
+        '
+        Me.btnMove.Location = New System.Drawing.Point(692, 8)
+        Me.btnMove.Name = "btnMove"
+        Me.btnMove.Size = New System.Drawing.Size(96, 41)
+        Me.btnMove.TabIndex = 4
+        Me.btnMove.Text = "Move Files "
+        Me.btnMove.UseVisualStyleBackColor = True
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(25, 69)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(96, 41)
+        Me.btnBrowse.TabIndex = 5
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'lsbResult
+        '
+        Me.lsbResult.FormattingEnabled = True
+        Me.lsbResult.Location = New System.Drawing.Point(25, 117)
+        Me.lsbResult.Name = "lsbResult"
+        Me.lsbResult.Size = New System.Drawing.Size(759, 316)
+        Me.lsbResult.TabIndex = 6
+        '
+        'btnAnalyse
+        '
+        Me.btnAnalyse.Location = New System.Drawing.Point(152, 70)
+        Me.btnAnalyse.Name = "btnAnalyse"
+        Me.btnAnalyse.Size = New System.Drawing.Size(96, 41)
+        Me.btnAnalyse.TabIndex = 7
+        Me.btnAnalyse.Text = "Analyse"
+        Me.btnAnalyse.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(692, 69)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(96, 41)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblAnalyseResult
+        '
+        Me.lblAnalyseResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAnalyseResult.Location = New System.Drawing.Point(254, 82)
+        Me.lblAnalyseResult.Name = "lblAnalyseResult"
+        Me.lblAnalyseResult.Size = New System.Drawing.Size(318, 26)
+        Me.lblAnalyseResult.TabIndex = 9
+        '
+        'SaveAnakysis
+        '
+        Me.SaveAnakysis.Location = New System.Drawing.Point(578, 70)
+        Me.SaveAnakysis.Name = "SaveAnakysis"
+        Me.SaveAnakysis.Size = New System.Drawing.Size(96, 41)
+        Me.SaveAnakysis.TabIndex = 10
+        Me.SaveAnakysis.Text = "Save Analysis"
+        Me.SaveAnakysis.UseVisualStyleBackColor = True
+        '
+        'frmDataStorage
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SaveAnakysis)
+        Me.Controls.Add(Me.lblAnalyseResult)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnAnalyse)
+        Me.Controls.Add(Me.lsbResult)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.btnMove)
+        Me.Controls.Add(Me.btnDirectory)
+        Me.Controls.Add(Me.txtInputDirectoryName)
+        Me.Controls.Add(Me.lblInfoDirectory)
+        Me.Name = "frmDataStorage"
+        Me.Text = "Data Storage"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblInfoDirectory As Label
+    Friend WithEvents txtInputDirectoryName As TextBox
+    Friend WithEvents btnDirectory As Button
+    Friend WithEvents btnMove As Button
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents lsbResult As ListBox
+    Friend WithEvents btnAnalyse As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblAnalyseResult As Label
+    Friend WithEvents SaveAnakysis As Button
+End Class
---
+++ Application 3/Form1.vb
@@ -0,0 +1,131 @@
+﻿Option Explicit On
+Option Strict On
+Imports System.IO
+Imports System.Text.RegularExpressions
+Public Class frmDataStorage
+    Dim fileName, fileName2 As String
+    Dim fileChooser As New OpenFileDialog
+    Dim robotFileReader As StreamReader
+    Dim robotFileWriter As StreamWriter
+    Dim directoryTarget, newLocation As String
+    'this part check if path has been not set before and set a new path
+    Sub fileSet()
+        'If fileName = "" Then
+        Dim result As DialogResult = fileChooser.ShowDialog()
+        If result = Windows.Forms.DialogResult.OK Then
+            fileName = fileChooser.FileName
+        End If
+        'End If
+    End Sub
+    'this part check if path has been not set before and set a new path
+    Sub fileSet2()
+        'If fileName2 = "" Then
+        Dim result As DialogResult = fileChooser.ShowDialog()
+        If result = Windows.Forms.DialogResult.OK Then
+            fileName2 = fileChooser.FileName
+        End If
+        'End If
+    End Sub
+    'this close active links 
+    Sub linkClose()
+        If robotFileWriter IsNot Nothing Then
+            robotFileWriter.Close()
+        End If
+        If robotFileReader IsNot Nothing Then
+            robotFileReader.Close()
+        End If
+    End Sub
+    ' this is where we open the list
+    Sub read()
+        Try
+            fileSet()
+            If fileName <> "" Then
+                linkClose()
+                lsbResult.Items.Clear()
+                robotFileReader = New StreamReader(fileName)
+                lsbResult.Items.Add(String.Format("--------- Opening {0} ------------", fileName))
+                lsbResult.Items.Add(" ")
+                Do While Not robotFileReader.EndOfStream
+                    lsbResult.Items.Add(robotFileReader.ReadLine)
+                Loop
+                linkClose()
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+    'closing the program here
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+    'when browse get clicked 
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        read()
+    End Sub
+    'this is where we create new directory 
+    Private Sub btnDirectory_Click(sender As Object, e As EventArgs) Handles btnDirectory.Click
+        Try
+            directoryTarget = txtInputDirectoryName.Text
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+        If directoryTarget <> "" Then
+            Try
+                My.Computer.FileSystem.CreateDirectory("C:\" & directoryTarget)
+                newLocation = "C:\" & directoryTarget
+                MsgBox("directory created")
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        Else
+            MsgBox("Please select a name for the new directory")
+        End If
+    End Sub
+    'this is where we move old files to new directory
+    Private Sub btnMove_Click(sender As Object, e As EventArgs) Handles btnMove.Click
+        If newLocation <> "" Then
+            Try
+                My.Computer.FileSystem.MoveFile("C:\baseball.txt", newLocation & "\baseball.txt")
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        Else
+            MsgBox("Please make a new directory first")
+        End If
+    End Sub
+    'this is where we analyse it
+    Private Sub btnAnalyse_Click(sender As Object, e As EventArgs) Handles btnAnalyse.Click
+        If fileName <> "" Then
+            Try
+                Dim text = File.ReadAllText(fileName)
+                Dim countIn = Regex.Matches(text, "in").Count
+                Dim countOut = Regex.Matches(text, "out").Count
+                lblAnalyseResult.Text = String.Format("Anylysis Result: occurrences of Word 'in' : {0} and Word 'out' : {1}", countIn, countOut)
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        Else
+            MsgBox("Please open a file for analysis first")
+        End If
+    End Sub
+    'this is where we save the analysis
+    Private Sub SaveAnakysis_Click(sender As Object, e As EventArgs) Handles SaveAnakysis.Click
+        Try
+            If lblAnalyseResult.Text <> "" Then
+                fileSet2()
+                If fileName2 <> "" Then
+                    linkClose()
+                    robotFileWriter = New StreamWriter(fileName2, True)
+                    robotFileWriter.WriteLine(lblAnalyseResult.Text)
+                End If
+            Else
+                MsgBox("Please do the analysis first")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+
+
+End Class
