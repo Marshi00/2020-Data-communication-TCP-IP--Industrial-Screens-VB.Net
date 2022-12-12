11f2d572748e856d14574ffa012c51a200897292
Marshi00 <112508169+Marshi00@users.noreply.github.com>
2022-12-12 11:14:11 -0500

    Add files via upload

---
+++ Structured 5/Form1.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRobotDatabase
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblInfoS = New System.Windows.Forms.Label()
+        Me.lblInfoMod = New System.Windows.Forms.Label()
+        Me.lblInfoMu = New System.Windows.Forms.Label()
+        Me.txtSerialNumber = New System.Windows.Forms.TextBox()
+        Me.txtModelNumber = New System.Windows.Forms.TextBox()
+        Me.txtManufacturer = New System.Windows.Forms.TextBox()
+        Me.cmbYearOfPurchase = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.grpStatus = New System.Windows.Forms.GroupBox()
+        Me.rdbDead = New System.Windows.Forms.RadioButton()
+        Me.rdbGood = New System.Windows.Forms.RadioButton()
+        Me.rdbNew = New System.Windows.Forms.RadioButton()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnRobotList = New System.Windows.Forms.Button()
+        Me.lsbResult = New System.Windows.Forms.ListBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.grpStatus.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblInfoS
+        '
+        Me.lblInfoS.Location = New System.Drawing.Point(12, 47)
+        Me.lblInfoS.Name = "lblInfoS"
+        Me.lblInfoS.Size = New System.Drawing.Size(197, 23)
+        Me.lblInfoS.TabIndex = 0
+        Me.lblInfoS.Text = "Please enter the robot serial number "
+        '
+        'lblInfoMod
+        '
+        Me.lblInfoMod.Location = New System.Drawing.Point(12, 116)
+        Me.lblInfoMod.Name = "lblInfoMod"
+        Me.lblInfoMod.Size = New System.Drawing.Size(197, 23)
+        Me.lblInfoMod.TabIndex = 1
+        Me.lblInfoMod.Text = "Please enter the robot model number"
+        '
+        'lblInfoMu
+        '
+        Me.lblInfoMu.Location = New System.Drawing.Point(12, 81)
+        Me.lblInfoMu.Name = "lblInfoMu"
+        Me.lblInfoMu.Size = New System.Drawing.Size(197, 23)
+        Me.lblInfoMu.TabIndex = 2
+        Me.lblInfoMu.Text = "Please enter the robot manufacturer "
+        '
+        'txtSerialNumber
+        '
+        Me.txtSerialNumber.Location = New System.Drawing.Point(247, 44)
+        Me.txtSerialNumber.Name = "txtSerialNumber"
+        Me.txtSerialNumber.Size = New System.Drawing.Size(221, 20)
+        Me.txtSerialNumber.TabIndex = 3
+        '
+        'txtModelNumber
+        '
+        Me.txtModelNumber.Location = New System.Drawing.Point(247, 113)
+        Me.txtModelNumber.Name = "txtModelNumber"
+        Me.txtModelNumber.Size = New System.Drawing.Size(221, 20)
+        Me.txtModelNumber.TabIndex = 4
+        '
+        'txtManufacturer
+        '
+        Me.txtManufacturer.Location = New System.Drawing.Point(247, 78)
+        Me.txtManufacturer.Name = "txtManufacturer"
+        Me.txtManufacturer.Size = New System.Drawing.Size(221, 20)
+        Me.txtManufacturer.TabIndex = 5
+        '
+        'cmbYearOfPurchase
+        '
+        Me.cmbYearOfPurchase.FormattingEnabled = True
+        Me.cmbYearOfPurchase.Location = New System.Drawing.Point(247, 155)
+        Me.cmbYearOfPurchase.Name = "cmbYearOfPurchase"
+        Me.cmbYearOfPurchase.Size = New System.Drawing.Size(121, 21)
+        Me.cmbYearOfPurchase.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(12, 158)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(197, 23)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Please enter the year of purchase"
+        '
+        'grpStatus
+        '
+        Me.grpStatus.Controls.Add(Me.rdbDead)
+        Me.grpStatus.Controls.Add(Me.rdbGood)
+        Me.grpStatus.Controls.Add(Me.rdbNew)
+        Me.grpStatus.Location = New System.Drawing.Point(247, 198)
+        Me.grpStatus.Name = "grpStatus"
+        Me.grpStatus.Size = New System.Drawing.Size(200, 133)
+        Me.grpStatus.TabIndex = 8
+        Me.grpStatus.TabStop = False
+        '
+        'rdbDead
+        '
+        Me.rdbDead.AutoSize = True
+        Me.rdbDead.Location = New System.Drawing.Point(15, 101)
+        Me.rdbDead.Name = "rdbDead"
+        Me.rdbDead.Size = New System.Drawing.Size(51, 17)
+        Me.rdbDead.TabIndex = 9
+        Me.rdbDead.TabStop = True
+        Me.rdbDead.Text = "Dead"
+        Me.rdbDead.UseVisualStyleBackColor = True
+        '
+        'rdbGood
+        '
+        Me.rdbGood.AutoSize = True
+        Me.rdbGood.Location = New System.Drawing.Point(15, 66)
+        Me.rdbGood.Name = "rdbGood"
+        Me.rdbGood.Size = New System.Drawing.Size(51, 17)
+        Me.rdbGood.TabIndex = 1
+        Me.rdbGood.TabStop = True
+        Me.rdbGood.Text = "Good"
+        Me.rdbGood.UseVisualStyleBackColor = True
+        '
+        'rdbNew
+        '
+        Me.rdbNew.AutoSize = True
+        Me.rdbNew.Location = New System.Drawing.Point(15, 32)
+        Me.rdbNew.Name = "rdbNew"
+        Me.rdbNew.Size = New System.Drawing.Size(47, 17)
+        Me.rdbNew.TabIndex = 0
+        Me.rdbNew.Text = "New"
+        Me.rdbNew.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(12, 267)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(197, 23)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Please select the robot status"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(26, 360)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(183, 78)
+        Me.btnAdd.TabIndex = 10
+        Me.btnAdd.Text = "Add Robot"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnRobotList
+        '
+        Me.btnRobotList.Location = New System.Drawing.Point(285, 360)
+        Me.btnRobotList.Name = "btnRobotList"
+        Me.btnRobotList.Size = New System.Drawing.Size(183, 78)
+        Me.btnRobotList.TabIndex = 11
+        Me.btnRobotList.Text = "Display Robot List"
+        Me.btnRobotList.UseVisualStyleBackColor = True
+        '
+        'lsbResult
+        '
+        Me.lsbResult.FormattingEnabled = True
+        Me.lsbResult.Location = New System.Drawing.Point(498, 44)
+        Me.lsbResult.Name = "lsbResult"
+        Me.lsbResult.Size = New System.Drawing.Size(349, 316)
+        Me.lsbResult.TabIndex = 12
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(581, 366)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(183, 78)
+        Me.btnExit.TabIndex = 13
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmRobotDatabase
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(859, 450)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lsbResult)
+        Me.Controls.Add(Me.btnRobotList)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.grpStatus)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbYearOfPurchase)
+        Me.Controls.Add(Me.txtManufacturer)
+        Me.Controls.Add(Me.txtModelNumber)
+        Me.Controls.Add(Me.txtSerialNumber)
+        Me.Controls.Add(Me.lblInfoMu)
+        Me.Controls.Add(Me.lblInfoMod)
+        Me.Controls.Add(Me.lblInfoS)
+        Me.Name = "frmRobotDatabase"
+        Me.Text = "Robot Database "
+        Me.grpStatus.ResumeLayout(False)
+        Me.grpStatus.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblInfoS As Label
+    Friend WithEvents lblInfoMod As Label
+    Friend WithEvents lblInfoMu As Label
+    Friend WithEvents txtSerialNumber As TextBox
+    Friend WithEvents txtModelNumber As TextBox
+    Friend WithEvents txtManufacturer As TextBox
+    Friend WithEvents cmbYearOfPurchase As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents grpStatus As GroupBox
+    Friend WithEvents rdbDead As RadioButton
+    Friend WithEvents rdbGood As RadioButton
+    Friend WithEvents rdbNew As RadioButton
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnRobotList As Button
+    Friend WithEvents lsbResult As ListBox
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Structured 5/Form1.vb
@@ -0,0 +1,180 @@
+﻿Option Explicit On
+Option Strict On
+Imports System.IO
+Public Class frmRobotDatabase
+    Dim validationCode As Integer
+    Dim robotStatus, fileName As String
+    Dim fileChooser As New OpenFileDialog
+    Dim robotFileReader As StreamReader
+    Dim robotFileWriter As StreamWriter
+    Dim newRobot As robot
+    'this is structure to store function and variables reltared to the robot
+    Public Structure robot
+        Dim serialNumber As String
+        Dim manufacturer As String
+        Dim modelNumber As String
+        Dim yearOfPurchase As Integer
+        Dim currentRobotStatus As String
+        'this check the waranth status of robot
+        Function warrantyCheck(ByVal inputstring As Integer) As String
+            Dim currentYear As Integer = System.DateTime.Now.Year
+            If (currentYear - inputstring) < 1 Then
+                warrantyCheck = "Valid"
+            Else
+                warrantyCheck = "Expired"
+            End If
+        End Function
+    End Structure
+    'this part check if path has been not set before and set a new path
+    Sub fileSet()
+        If fileName = "" Then
+            Dim result As DialogResult = fileChooser.ShowDialog()
+            If result = Windows.Forms.DialogResult.OK Then
+                fileName = fileChooser.FileName
+            End If
+        End If
+    End Sub
+    'this part check the status of robot
+    Private Sub rdbNew_CheckedChanged(sender As Object, e As EventArgs) Handles rdbNew.CheckedChanged
+        If rdbNew.Checked = True Then
+            robotStatus = "New"
+        End If
+    End Sub
+    Private Sub rdbDead_CheckedChanged(sender As Object, e As EventArgs) Handles rdbDead.CheckedChanged
+        If rdbDead.Checked = True Then
+            robotStatus = "Dead"
+        End If
+    End Sub
+    Private Sub rdbGood_CheckedChanged(sender As Object, e As EventArgs) Handles rdbGood.CheckedChanged
+        If rdbGood.Checked = True Then
+            robotStatus = "Good"
+        End If
+    End Sub
+    'this add items in the combo box on form load
+    Private Sub frmRobotDatabase_Load(sender As Object, e As EventArgs) Handles Me.Load
+        For yearStart As Integer = 2016 To 2020
+            cmbYearOfPurchase.Items.Add(yearStart)
+        Next
+    End Sub
+    'this function checks the inputs are valid
+    Function validation(ByVal input1 As String, ByVal input2 As String, ByVal input3 As String, ByVal input4 As String, ByVal input5 As String) As Integer
+        Dim validationResult As Integer
+        'Checking inputs are not empty
+        If input1 <> "" Then
+            If input2 <> "" Then
+                If input3 <> "" Then
+                    If input4 <> "" Then
+                        If input5 <> "" Then
+                            'this part checking the input is not numeric
+                            If IsNumeric(input4) Then
+                                newRobot.yearOfPurchase = CInt(input4)
+                                'this part checking yearOfPurchase is not over current date
+                                If newRobot.yearOfPurchase > System.DateTime.Now.Year Then
+                                    validationResult = 7
+                                Else
+                                    validationResult = 8
+                                    newRobot.serialNumber = input1
+                                    newRobot.manufacturer = input2
+                                    newRobot.modelNumber = input3
+                                    newRobot.currentRobotStatus = input5
+                                End If
+                            Else
+                                validationResult = 6
+                            End If
+                        Else
+                            validationResult = 5
+                        End If
+                    Else
+                        validationResult = 4
+                    End If
+                Else
+                    validationResult = 3
+                End If
+            Else
+                validationResult = 2
+            End If
+        Else
+            validationResult = 1
+        End If
+        validation = validationResult
+    End Function
+    'This part is to show the needed Error Msg
+    Sub errorMsg(ByVal inputMsgCode As Integer)
+        Dim msgNote As String = ""
+        Select Case inputMsgCode
+            Case 8
+                msgNote = "all values are valid"
+            Case 1
+                msgNote = "Please enter the robot serial number"
+            Case 2
+                msgNote = "Please enter the Manufacturer"
+            Case 3
+                msgNote = "Please enter the Model Number "
+            Case 4
+                msgNote = "Please enter the year of purchase"
+            Case 5
+                msgNote = "Please select the robot status"
+            Case 6
+                msgNote = " Please enter the year of purchase in numeric format"
+            Case 7
+                msgNote = " Year of purchase can not be over current date "
+        End Select
+        MsgBox(msgNote)
+    End Sub
+    'this clearout everything
+    Sub clear()
+        txtManufacturer.Text = ""
+        txtModelNumber.Text = ""
+        txtSerialNumber.Text = ""
+        cmbYearOfPurchase.Text = ""
+        rdbDead.Checked = False
+        rdbGood.Checked = False
+        rdbNew.Checked = False
+        robotStatus = ""
+    End Sub
+    'this close active links 
+    Sub linkClose()
+        If robotFileWriter IsNot Nothing Then
+            robotFileWriter.Close()
+        End If
+        If robotFileReader IsNot Nothing Then
+            robotFileReader.Close()
+        End If
+    End Sub
+    'this close the program
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+    'this is where add button get clicked 
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        'checking that inputs are valid
+        validationCode = validation(txtSerialNumber.Text, txtManufacturer.Text, txtModelNumber.Text, cmbYearOfPurchase.Text, robotStatus)
+        If validationCode = 8 Then
+            fileSet()
+            If fileName <> "" Then
+                    linkClose()
+                    robotFileWriter = New StreamWriter(fileName, True)
+                robotFileWriter.WriteLine(String.Format("{0}, {1}, {2}, {3}, {4},{5}", newRobot.serialNumber, newRobot.manufacturer, newRobot.modelNumber, newRobot.yearOfPurchase, newRobot.warrantyCheck(newRobot.yearOfPurchase), newRobot.currentRobotStatus))
+                linkClose()
+                    clear()
+                End If
+
+            Else
+                errorMsg(validationCode)
+        End If
+    End Sub
+    'this is where display list button get clicked 
+    Private Sub btnRobotList_Click(sender As Object, e As EventArgs) Handles btnRobotList.Click
+        fileSet()
+        If fileName <> "" Then
+            linkClose()
+            robotFileReader = New StreamReader(fileName)
+            lsbResult.Items.Add(String.Format("--------- Opening {0} ------------", fileName))
+            lsbResult.Items.Add(" ")
+            Do While Not robotFileReader.EndOfStream
+                lsbResult.Items.Add(robotFileReader.ReadLine)
+            Loop
+            linkClose()
+        End If
+    End Sub
+End Class
