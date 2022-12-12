12ef6071050d051bd3e267414274eec05febaece
Marshi00 <112508169+Marshi00@users.noreply.github.com>
2022-12-12 11:13:09 -0500

    Add files via upload

---
+++ Structured 4/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmStringManipulation
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblInfo = New System.Windows.Forms.Label()
+        Me.txtUserInfo = New System.Windows.Forms.TextBox()
+        Me.btnConvertUpper = New System.Windows.Forms.Button()
+        Me.btnReverse = New System.Windows.Forms.Button()
+        Me.btnWordNumber = New System.Windows.Forms.Button()
+        Me.btnStudentNumber = New System.Windows.Forms.Button()
+        Me.btnAge = New System.Windows.Forms.Button()
+        Me.btnSwap = New System.Windows.Forms.Button()
+        Me.btnTotal = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblInfo
+        '
+        Me.lblInfo.Location = New System.Drawing.Point(45, 29)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(152, 21)
+        Me.lblInfo.TabIndex = 0
+        Me.lblInfo.Text = "Please enter your information"
+        '
+        'txtUserInfo
+        '
+        Me.txtUserInfo.Location = New System.Drawing.Point(200, 26)
+        Me.txtUserInfo.Name = "txtUserInfo"
+        Me.txtUserInfo.Size = New System.Drawing.Size(279, 20)
+        Me.txtUserInfo.TabIndex = 1
+        '
+        'btnConvertUpper
+        '
+        Me.btnConvertUpper.Location = New System.Drawing.Point(48, 120)
+        Me.btnConvertUpper.Name = "btnConvertUpper"
+        Me.btnConvertUpper.Size = New System.Drawing.Size(149, 62)
+        Me.btnConvertUpper.TabIndex = 3
+        Me.btnConvertUpper.Text = "Uppercase Convert"
+        Me.btnConvertUpper.UseVisualStyleBackColor = True
+        '
+        'btnReverse
+        '
+        Me.btnReverse.Location = New System.Drawing.Point(330, 120)
+        Me.btnReverse.Name = "btnReverse"
+        Me.btnReverse.Size = New System.Drawing.Size(149, 62)
+        Me.btnReverse.TabIndex = 4
+        Me.btnReverse.Text = "Reverse  Convert"
+        Me.btnReverse.UseVisualStyleBackColor = True
+        '
+        'btnWordNumber
+        '
+        Me.btnWordNumber.Location = New System.Drawing.Point(330, 293)
+        Me.btnWordNumber.Name = "btnWordNumber"
+        Me.btnWordNumber.Size = New System.Drawing.Size(149, 62)
+        Me.btnWordNumber.TabIndex = 5
+        Me.btnWordNumber.Text = "Number of words"
+        Me.btnWordNumber.UseVisualStyleBackColor = True
+        '
+        'btnStudentNumber
+        '
+        Me.btnStudentNumber.Location = New System.Drawing.Point(330, 207)
+        Me.btnStudentNumber.Name = "btnStudentNumber"
+        Me.btnStudentNumber.Size = New System.Drawing.Size(149, 62)
+        Me.btnStudentNumber.TabIndex = 6
+        Me.btnStudentNumber.Text = "Student Number"
+        Me.btnStudentNumber.UseVisualStyleBackColor = True
+        '
+        'btnAge
+        '
+        Me.btnAge.Location = New System.Drawing.Point(48, 207)
+        Me.btnAge.Name = "btnAge"
+        Me.btnAge.Size = New System.Drawing.Size(149, 62)
+        Me.btnAge.TabIndex = 7
+        Me.btnAge.Text = "Age"
+        Me.btnAge.UseVisualStyleBackColor = True
+        '
+        'btnSwap
+        '
+        Me.btnSwap.Location = New System.Drawing.Point(48, 293)
+        Me.btnSwap.Name = "btnSwap"
+        Me.btnSwap.Size = New System.Drawing.Size(149, 62)
+        Me.btnSwap.TabIndex = 8
+        Me.btnSwap.Text = "NameSwap"
+        Me.btnSwap.UseVisualStyleBackColor = True
+        '
+        'btnTotal
+        '
+        Me.btnTotal.Location = New System.Drawing.Point(48, 376)
+        Me.btnTotal.Name = "btnTotal"
+        Me.btnTotal.Size = New System.Drawing.Size(149, 62)
+        Me.btnTotal.TabIndex = 9
+        Me.btnTotal.Text = "Total  numerical values "
+        Me.btnTotal.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(77, 68)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(347, 32)
+        Me.lblResult.TabIndex = 10
+        Me.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(330, 376)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(149, 62)
+        Me.btnExit.TabIndex = 11
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmStringManipulation
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(512, 450)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnTotal)
+        Me.Controls.Add(Me.btnSwap)
+        Me.Controls.Add(Me.btnAge)
+        Me.Controls.Add(Me.btnStudentNumber)
+        Me.Controls.Add(Me.btnWordNumber)
+        Me.Controls.Add(Me.btnReverse)
+        Me.Controls.Add(Me.btnConvertUpper)
+        Me.Controls.Add(Me.txtUserInfo)
+        Me.Controls.Add(Me.lblInfo)
+        Me.Name = "frmStringManipulation"
+        Me.Text = "String Manipulation "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblInfo As Label
+    Friend WithEvents txtUserInfo As TextBox
+    Friend WithEvents btnConvertUpper As Button
+    Friend WithEvents btnReverse As Button
+    Friend WithEvents btnWordNumber As Button
+    Friend WithEvents btnStudentNumber As Button
+    Friend WithEvents btnAge As Button
+    Friend WithEvents btnSwap As Button
+    Friend WithEvents btnTotal As Button
+    Friend WithEvents lblResult As Label
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Structured 4/Form1.vb
@@ -0,0 +1,89 @@
+﻿Option Explicit On
+Option Strict On
+Imports System.Text.RegularExpressions
+Public Class frmStringManipulation
+    'this part convert the input to uppercase
+    Function upCase(ByVal inputString As String) As String
+        upCase = UCase(inputString)
+    End Function
+    'this part reverse the entire string 
+    Function reverse(ByVal inputString As String) As String
+        reverse = StrReverse(inputString)
+    End Function
+
+    'this is to check the number of words 
+    Function wordNumber(ByVal inputString As String) As Integer
+        Dim numberofWords As MatchCollection = Regex.Matches(inputString, "\S+")
+        Return numberofWords.Count
+        ' wordNumber = UBound(Split(Trim(Replace(inputString, Space(2), Space(1))))) + 1 ---> this logic is not giving results on ( , or other signs  )so  it's fail we could fix , by replace it with "" which is nothing  to issue a temprary fix but other signs still work
+        'this is another outside source that seem to work fine -->  
+    End Function
+    'this is to check for student number only by splitting using (,)
+    Function splitter(ByVal inputstring As String, ByVal neededInfo As Integer) As String
+        Dim word() As String = Split(Trim(inputstring), ",")
+        splitter = word(neededInfo)
+    End Function
+    ' this is to get the age of the user 
+    Function ageCalculator(ByVal inputstring As String) As Integer
+        Dim currentYear As Integer = System.DateTime.Now.Year
+        ageCalculator = currentYear - CInt(splitter(inputstring, 4))
+    End Function
+    'this is to swap first and last name 
+    Function nameSwap(ByVal inputstring As String) As String
+        Dim word() As String = Split(Trim(splitter(inputstring, 0)), " ")
+        nameSwap = String.Format("{1} {0}", word(0), word(1))
+    End Function
+    'this is to calculate all the nemrical values 
+    Function numericalCalculator(ByVal inputstring As String) As Integer
+        inputstring = inputstring.Replace(" ", "")
+        For counter As Integer = 0 To inputstring.Length - 1
+            numericalCalculator += CInt(inputstring.Substring(counter, 1))
+        Next counter
+    End Function
+
+
+    'this is where total numeric value button get clicked 
+    Private Sub btnTotal_Click(sender As Object, e As EventArgs) Handles btnTotal.Click
+        lblResult.Text = String.Format("the  total of all degits in the string is  {0} ", numericalCalculator(splitter(txtUserInfo.Text, 1)) + numericalCalculator(splitter(txtUserInfo.Text, 4)))
+    End Sub
+    'this is when swap button get clicked ( swapping first and last name positions)
+    Private Sub btnSwap_Click(sender As Object, e As EventArgs) Handles btnSwap.Click
+        lblResult.Text = String.Format("{4}, {0}, {1}, {2}, {3} ", splitter(txtUserInfo.Text, 1), splitter(txtUserInfo.Text, 2), splitter(txtUserInfo.Text, 3), splitter(txtUserInfo.Text, 4), nameSwap(txtUserInfo.Text))
+    End Sub
+
+    'this is when age button get clicked
+    Private Sub btnAge_Click(sender As Object, e As EventArgs) Handles btnAge.Click
+        lblResult.Text = String.Format("you are {0} years old ", ageCalculator(txtUserInfo.Text))
+    End Sub
+
+    'this is where student number button get clicked
+    Private Sub btnStudentNumber_Click(sender As Object, e As EventArgs) Handles btnStudentNumber.Click
+        lblResult.Text = String.Format("your student number is {0}", splitter(txtUserInfo.Text, 1))
+    End Sub
+
+    'this is where number of words button get clicked 
+    Private Sub btnWordNumber_Click(sender As Object, e As EventArgs) Handles btnWordNumber.Click
+        lblResult.Text = String.Format("There are {0} words", wordNumber(txtUserInfo.Text))
+    End Sub
+
+    'this is where reverse button get clicked 
+    Private Sub btnReverse_Click(sender As Object, e As EventArgs) Handles btnReverse.Click
+
+        lblResult.Text = String.Format("{0}", StrReverse(txtUserInfo.Text))
+
+    End Sub
+
+    'this is where upperCase button get clicked
+    Private Sub btnConvertUpper_Click(sender As Object, e As EventArgs) Handles btnConvertUpper.Click
+
+        lblResult.Text = String.Format("{0}", upCase(txtUserInfo.Text))
+
+    End Sub
+
+    'this close the program 
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+
+End Class
