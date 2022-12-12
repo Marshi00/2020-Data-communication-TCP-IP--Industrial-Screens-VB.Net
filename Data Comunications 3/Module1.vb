0edc7ec4b5800c484b310fc845722db294440880
Marshi00 <112508169+Marshi00@users.noreply.github.com>
2022-12-12 10:16:56 -0500

    Add files via upload

---
+++ Data Comunications 3/Module1.vb
@@ -0,0 +1,130 @@
+﻿Imports System.Net.Sockets
+Imports System.IO
+Module Module1
+    Dim client As System.Net.Sockets.TcpClient = New System.Net.Sockets.TcpClient("ftp.drivehq.com", 21)
+    Dim NS As NetworkStream = client.GetStream()
+    Dim sw As New StreamWriter(NS)
+    'm!47wW8HMJpewTG
+
+    'Reading function
+    Function ReadText(NS As NetworkStream) As String
+        Try
+            Do
+                ReadText = ReadText & Chr(NS.ReadByte)
+            Loop While NS.DataAvailable
+
+            Console.WriteLine(ReadText)
+        Catch ex As Exception
+            Console.WriteLine(ex)
+        End Try
+    End Function
+
+    'Data Connection Function 
+    Function dataStream(sw As StreamWriter, NS As NetworkStream) As NetworkStream
+        Try
+
+            sw.WriteLine("PASV")
+            Dim strPASV As String = ReadText(NS)
+            'Console.Write(strPASV)
+            Dim pasvArr() = Split(strPASV, ",")
+            Dim strHighByte As String = pasvArr(4)
+            Dim strLowByte As String = Split(pasvArr(5), ")")(0)
+            Dim DataPort As Integer = strLowByte + 256 * strHighByte
+            Dim dataClient As System.Net.Sockets.TcpClient = New System.Net.Sockets.TcpClient("ftp.drivehq.com", DataPort)
+
+            dataStream = dataClient.GetStream()
+
+        Catch ex As Exception
+            Console.WriteLine(ex)
+        End Try
+    End Function
+
+    Sub Main()
+        sw.AutoFlush = True
+        Console.WriteLine("Made connection, hit enter to continue")
+        Console.ReadLine()
+
+        'Reading the first response from the server 
+
+        ReadText(NS)
+
+        'Taking the User Name 
+        Console.WriteLine("Please Enter your user Name ")
+        Dim userName As String = Console.ReadLine()
+        sw.WriteLine("USER " & userName)
+
+        ReadText(NS)
+
+        'Taking the password 
+
+        Console.WriteLine("Please Enter your PassWord ")
+        Dim passWord As String = Console.ReadLine()
+        sw.WriteLine("PASS " & passWord)
+
+        ReadText(NS)
+
+        'Starting Data connection and getting LIST
+
+        Dim dataNS As NetworkStream = dataStream(sw, NS)
+        sw.WriteLine("LIST")
+        ReadText(NS)
+        ReadText(dataNS)
+        ReadText(NS)
+        dataNS.Close()
+
+
+
+        'Starting Data connection and getting NLST
+        dataNS = dataStream(sw, NS)
+        sw.WriteLine("NLST")
+        ReadText(NS)
+        ReadText(dataNS)
+        ReadText(NS)
+        dataNS.Close()
+
+
+        'Starting Data coneection ang getting RETR 
+        dataNS = dataStream(sw, NS)
+        sw.WriteLine("RETR Smile.png")
+        ReadText(NS)
+        Dim fileStream As FileStream = New FileStream("D:\MySmile.png", FileMode.Create)
+
+        Try
+
+            Do
+                Dim retrByte As Byte = dataNS.ReadByte()
+                Console.Write(retrByte)
+                fileStream.WriteByte(retrByte)
+            Loop While dataNS.DataAvailable
+
+        Catch ex As Exception
+            Console.WriteLine(ex)
+        End Try
+
+        ReadText(NS)
+        dataNS.Close()
+
+        'Starting Data Connection and Sending an image
+        dataNS = dataStream(sw, NS)
+        sw.WriteLine("STOR Frown.png")
+        ReadText(NS)
+        Dim fsSource As FileStream = New FileStream("D:\Frown.png", FileMode.Open, FileAccess.Read)
+        Try
+
+            Do Until fsSource.Length = fsSource.Position
+                Dim rByte As Byte = fsSource.ReadByte
+                dataNS.WriteByte(rByte)
+            Loop
+        Catch ex As Exception
+            Console.WriteLine(ex)
+        End Try
+
+        ReadText(NS)
+
+        dataNS.Close()
+        Console.WriteLine("DONE")
+        Console.ReadLine()
+    End Sub
+
+
+End Module
---
+++ Data Comunications 3/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Lab3-MohammadArshian")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Lab3-MohammadArshian")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("34dc6af0-1b5f-4b29-a6d2-f8e03f0b30ea")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
